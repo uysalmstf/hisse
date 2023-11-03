aae5d8a0f0a646b2f44f8de891b785956b628e48
Mustafa UYSAL <mustafa-uysal@outlook.com>
2023-11-03 23:04:21 +0300

    sequalize

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
 const bodyParser = require('body-parser');
-
+const sequelize = require('./config/sequalize')
 app.use(bodyParser.urlencoded({extended: false}))
 
 dotenv.config()
 
+sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+ }).catch((error) => {
+    console.error('Unable to connect to the database: ', error);
+ });
+ 
 const port = process.env.PORT
 
 const userRoutes = require('./routes/UserRoutes')
