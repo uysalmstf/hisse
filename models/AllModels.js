855d315a7b5209385b64b446f12e003831fad187
Mustafa UYSAL <mustafa-uysal@outlook.com>
2023-11-06 01:01:23 +0300

    create bulk data fixed

--- models/AllModels.js
+++ models/AllModels.js
@@ -58,6 +58,24 @@ const Share = sequelize.define("share", {
   }
 });
 
+
+//burada sadece hisseleri olusturdum. Diger islemleri API Endpointleri yardimiyla yapabilirsiniz.
+const ShareData = [
+  { code: 'ABC', name: 'Deneme 1', buy_price: 1.50, sell_price: 2.50, count: 100 },
+  { code: 'DEF', name: 'Deneme 2', buy_price: 2.50, sell_price: 3.50, count: 200 },
+  { code: 'GHI', name: 'Deneme 3', buy_price: 3.50, sell_price: 4.50, count: 300 },
+  { code: 'JKL', name: 'Deneme 4', buy_price: 4.50, sell_price: 5.50, count: 400 }
+
+];
+
+Share.bulkCreate(ShareData)
+  .then((users) => {
+    console.log('Bulk data insertion successful');
+  })
+  .catch((error) => {
+    console.error('Error inserting bulk data: ' + error.message);
+  });
+
  Share.belongsToMany(UserPortfolio, {through: SharePortfolio})
  UserPortfolio.belongsToMany(Share, {through: SharePortfolio})
  Share.hasMany(SharePortfolioLogs)
